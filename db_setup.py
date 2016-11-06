7fe6266362f4e1311e731da18c946c0a30edb620
Enrique Garcia <egarcialopez@gmail.com>
2016-11-06 12:40:57 +0100

    First running version of app

--- db_setup.py
+++ db_setup.py
@@ -4,16 +4,16 @@ connection = pymysql.connect(host="localhost", user=dbconfig.db_user, passwd=dbc
 
 try:
     with connection.cursor() as cursor:
-        sql = "CREATE DATABASE IF NOT EXISTS crimemap"
+        sql = "CREATE DATABASE IF NOT EXISTS crimesmap"
         cursor.execute(sql)
-        sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes(
+        sql = """CREATE TABLE IF NOT EXISTS crimesmap.crimes(
         id int NOT NULL AUTO_INCREMENT,
         latitude FLOAT(10,6),
         longitude FLOAT(10,6),
         date DATETIME,
         category VARCHAR(50),
         description VARCHAR(1000),
-        updated_at TIMESTAMPS,
+        updated_at TIMESTAMP,
         PRIMARY KEY (id)
         )"""
         cursor.execute(sql)
