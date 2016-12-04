b86719ed3a4a347d20c80679a940ae0007da568f
Enrique Garcia <egarcialopez@gmail.com>
2016-12-05 00:05:39 +0100

    POST to DB

--- dbhelper.py
+++ dbhelper.py
@@ -1,15 +1,16 @@
 import pymysql
 import dbconfig
+import datetime
+
 
 class DBHelper:
-
-    def connect (self, database="crimesmap"):
+    def connect(self, database="crimesmap"):
         return pymysql.connect(host="localhost", user=dbconfig.db_user, passwd=dbconfig.db_password, db=database)
 
     def get_all_inputs(self):
-        connection=self.connect()
+        connection = self.connect()
         try:
-            query="SELECT description FROM crimes;"
+            query = "SELECT description FROM crimes;"
             with connection.cursor() as cursor:
                 cursor.execute(query)
             return cursor.fetchall()
@@ -26,7 +27,6 @@ class DBHelper:
         finally:
             connection.close()
 
-
     def clear_all(self):
         connection = self.connect()
         try:
@@ -34,5 +34,36 @@ class DBHelper:
             with connection.cursor() as cursor:
                 cursor.execute(query)
                 connection.commit()
+        finally:
+            connection.close()
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (category, date, latitude, longitude, description) VALUES (%s, %s, %s, %s, %s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (category, date, latitude, longitude, description))
+                connection.commit()
+        finally:
+            connection.close()
+
+    def get_all_crimes(self):
+        connection = self.connect()
+        try:
+            query = "SELECT latitude, longitude, date, category, description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            named_crimes = []
+            for crime in cursor:
+                named_crime = {
+                    'latitude': crime[0],
+                    'longitude': crime[1],
+                    'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+                    'category': crime[3],
+                    'description': crime[4]
+                }
+                named_crimes.append(named_crime)
+            print(named_crimes)
+            return named_crimes
         finally:
             connection.close()
